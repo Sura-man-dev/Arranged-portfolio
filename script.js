8304f8810ff26929c8c7c3e903dc63e50bad1df0
surafel <rohobotsolomon54@gmail.com>
2025-10-28 11:23:03 -0700

    Update all code.

--- script.js
+++ script.js
@@ -4,4 +4,22 @@ var typed = new Typed(".text", {
     backSpeed: 100,
     backDelay: 1000,
     loop: true
+})
+
+const menuToggle = document.getElementById('menu-toggle');
+const navbar = document.getElementById('navbar');
+
+menuToggle.addEventListener('click', () => {
+    navbar.classList.toggle('active');
 });
+
+
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', () => {
+        navbar.classList.remove('active');
+    });
+});
+
+
+
+
